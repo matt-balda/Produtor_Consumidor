61b136b72a4501d1f4cba0a4d3ad5556f182df3f
matt-balda <mateusbalda89@gmail.com>
2022-11-10 17:46:50 -0300

    add method processar

--- Produtor_Consumidor/src/ProcessadorThread.java
+++ Produtor_Consumidor/src/ProcessadorThread.java
@@ -8,4 +8,13 @@ public class ProcessadorThread extends Thread{
         this.idThread = id;
         this.semaphore = semaphore;
     }
+
+    private void processar() {
+        try {
+            System.out.println("Thread #"+idThread+" processando");
+            Thread.sleep((long) (Math.random() * 10000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
